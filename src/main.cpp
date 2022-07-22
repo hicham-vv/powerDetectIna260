06c46c1cf87efe120f8328cd4477be6e686c529c
hicham-vv <77798347+hicham-vv@users.noreply.github.com>
2022-07-22 09:29:49 +0100

    update version

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Adafruit_INA260.h>
 #include <math.h>
 #include "SparkFunSX1509.h"
+#include "esp_task_wdt.h"
 
 
   /******** BLock SX01 ********/
@@ -104,8 +105,8 @@ typedef struct message {
   // char a[10];
 
   char Nsensor[cSize+1]= {'0','0','0','0','0','0','0','0',
-                        '0','0','0','0','0','0','0','0',
-                        '0','0','0','0','0','0','0','0'};
+                          '0','0','0','0','0','0','0','0',
+                          '0','0','0','0','0','0','0','0'};
 }message;
 message bus; // créer une structure message nommé bus
 
@@ -114,8 +115,8 @@ bool voltage=false;
 
 
 char refPD[cSize+1]= {'0','0','0','0','0','0','0','0',
-                    '0','0','0','0','0','0','0','0',
-                    '0','0','0','0','0','0','0','0'};
+                      '0','0','0','0','0','0','0','0',
+                      '0','0','0','0','0','0','0','0'};
 int refWaterLV=0;
 bool send=false;
 
@@ -157,6 +158,9 @@ void OnDataSent(const uint8_t *mac, esp_now_send_status_t status) {
 }
 
 void setup() {
+  delay(500);
+  esp_task_wdt_init(25, true); //enable panic so ESP32 restarts
+  esp_task_wdt_add(NULL); //add current thread to WDT watch
   #ifdef debug
   Serial.begin(115200);
   while(!Serial);
@@ -169,6 +173,9 @@ void setup() {
   delay(1500);
   pinMode(Reg_Enable,INPUT);
   delay(500);
+
+  esp_task_wdt_reset();
+
   if(!Wire.begin(21,22,100000)){
     blinkLed(1000,25); // for debugging
     #ifdef debug
@@ -195,6 +202,7 @@ void setup() {
     #endif
   }
 
+  esp_task_wdt_reset();
   delay(500);
   if (!io_2.begin(SX1509_ADDRESS_2)){
     #ifdef debug
@@ -236,6 +244,7 @@ void setup() {
 
 
 
+  esp_task_wdt_reset();
 
   
 
@@ -273,6 +282,7 @@ void setup() {
       esp_restart();
     }
   }
+  esp_task_wdt_reset();
 
 void loop() {
 
@@ -326,11 +336,15 @@ void loop() {
   }
   if(compteur>2){
     bus.Nsensor[COM+16]='1';
+    #ifdef debug
     Serial.println("Compactation ON");
+    #endif
 
   }else{
     bus.Nsensor[COM+16]='0';
+    #ifdef debug
     Serial.println("Compactation OFF\n");
+    #endif
   }
 
   
@@ -342,7 +356,9 @@ void loop() {
   }
   if(send){
     send=false;
+    #ifdef debug
     Serial.println("\nSending Data\n");
+    #endif
     for(int i=0;i<3;i++){
       sendData();
       delay(2500);
@@ -353,7 +369,10 @@ void loop() {
       }
     }
   }
-  delay(2500);
+  #ifdef debug
+  Serial.println("*******************************");
+  #endif
+  delay(1500);
   #endif
 
   // #ifdef Citerne
@@ -519,46 +538,46 @@ void loop() {
   voltage=io_1.digitalRead(BC);
   if(!voltage) bus.Nsensor[BC]='1'; else bus.Nsensor[BC]='0';
 
-      voltage=io_1.digitalRead(AC);
+  voltage=io_1.digitalRead(AC);
   if(!voltage) bus.Nsensor[AC]='1'; else bus.Nsensor[AC]='0';
 
-      voltage=io_1.digitalRead(LC);
+  voltage=io_1.digitalRead(LC);
   if(!voltage) bus.Nsensor[LC]='1'; else bus.Nsensor[LC]='0';
 
-      voltage=io_1.digitalRead(AMA);
+  voltage=io_1.digitalRead(AMA);
   if(!voltage) bus.Nsensor[AMA]='1'; else bus.Nsensor[AMA]='0';
 
-      voltage=io_1.digitalRead(AP);
+  voltage=io_1.digitalRead(AP);
   if(!voltage) bus.Nsensor[AP]='1'; else bus.Nsensor[AP]='0';
 
-      voltage=io_1.digitalRead(ARD);
+  voltage=io_1.digitalRead(ARD);
   if(!voltage) bus.Nsensor[ARD]='1'; else bus.Nsensor[ARD]='0';
 
-      voltage=io_1.digitalRead(K1);
+  voltage=io_1.digitalRead(K1);
   if(!voltage) bus.Nsensor[K1]='1'; else bus.Nsensor[K1]='0';
 
-      voltage=io_1.digitalRead(K2);
+  voltage=io_1.digitalRead(K2);
   if(!voltage) bus.Nsensor[K2]='1'; else bus.Nsensor[K2]='0';
 
-      voltage=io_1.digitalRead(LAB);
+  voltage=io_1.digitalRead(LAB);
   if(!voltage) bus.Nsensor[LAB]='1'; else bus.Nsensor[LAB]='0';
 
-      voltage=io_1.digitalRead(VES);
+  voltage=io_1.digitalRead(VES);
   if(!voltage) bus.Nsensor[VES]='1'; else bus.Nsensor[VES]='0';
 
-      voltage=io_1.digitalRead(OPA);
+  voltage=io_1.digitalRead(OPA);
   if(!voltage) bus.Nsensor[OPA]='1'; else bus.Nsensor[OPA]='0';
 
 
   /**************************SX02************************************/
 
-      voltage=io_2.digitalRead(CLC);
+  voltage=io_2.digitalRead(CLC);
   if(!voltage) bus.Nsensor[CLC+16]='1'; else bus.Nsensor[CLC+16]='0';
 
-      voltage=io_2.digitalRead(COM);
+  voltage=io_2.digitalRead(COM);
   if(!voltage) bus.Nsensor[COM+16]='1'; else bus.Nsensor[COM+16]='0';
 
-        voltage=io_2.digitalRead(PT);
+  voltage=io_2.digitalRead(PT);
   if(!voltage) bus.Nsensor[PT+16]='1'; else bus.Nsensor[PT+16]='0';
 
   
@@ -575,7 +594,10 @@ void loop() {
   #endif
 
   delay(2500);
-  #endif
+#endif
+
+  esp_task_wdt_reset();
+
 }
 
 
