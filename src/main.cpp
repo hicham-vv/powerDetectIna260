7b385ec3b736a10cefba9fed1ac77578f3b305fe
hicham-vv <77798347+hicham-vv@users.noreply.github.com>
2023-05-29 13:47:26 +0000

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <esp_now.h>
 #include <Adafruit_INA260.h>
+#include <esp_wifi.h>
 
+#include "esp_task_wdt.h"
 #ifdef ESP32
   #include <WiFi.h>
 #endif
@@ -17,18 +19,21 @@
 
 
 
+// #define FuelLV
+
 #define TensionSeuil 10
 
-uint8_t receiverMAC[] = {0x4c,0x11,0xae,0x9d,0x6e,0x6c}; // TracCar MAC Adress 4c:11:ae:9d:6e:6c
 
 
 
+uint8_t selfMAC[] = {0x00, 0xBB, 0x00, 0x00, 0x98, 0x00}; // Mac adress de la carte Mére lui même 
+uint8_t receiverMAC[] = {0x00, 0xAA, 0x00, 0x00, 0x98, 0x00}; // Master MAC Adress 
 
 
 
 Adafruit_INA260 ina260_1 = Adafruit_INA260();
 
-
+esp_now_peer_info_t peerInfo;
 
 #define Led_esp 2  
 boolean ledState = false;
@@ -48,15 +53,26 @@ unsigned long currentMillis;
 unsigned long sentStartTime;
 unsigned long lastSentTime;
 
+#define cSize 24 // Trame Powerdetect size
+
 typedef struct message {
-char PD1='0';
-char PD2='0';
-char PD3='0';
-int TotalDistance = -1;
-int FuelTank = -1;
-int TotalHours = -1;
-int TotalFuelused =-1;
-int CoolantTemp =-1;
+  char V1 = '0';
+  char V2 = '0';
+  char V3 = '0';
+  int TotalDistance =-1;
+  int FuelTank =-1;
+  int TotalHours =-1;
+  int TotalFuelused =-1;
+  int CoolantTemp =-1;
+  int RPM =-1;
+  int BrakePP=-1;
+  int AccPP=-1;
+  // uint8_t *buffer;
+  char Nsensor[cSize+1]= {'0','0','0','0','0','0','0','0',
+                          '0','0','0','0','0','0','0','0',
+                          '0','0','0','0','0','0','0','0'};
+  int CAN9=-1;
+  int weight=-1;
 }message;
 message bus; // créer une structure message nommé bus
 
@@ -65,6 +81,11 @@ int voltage=0;
 char refPD1='0';
 bool send=false;
 bool SendOK=false;
+
+
+
+  int waterlv=0;
+  int Mwaterlv=0;
 
 
 
@@ -95,6 +116,11 @@ void OnDataSent(const uint8_t *mac, esp_now_send_status_t status) {
 
 
 void setup() {
+
+
+  esp_task_wdt_init(60, true); //enable panic so ESP32 restarts
+  esp_task_wdt_add(NULL); //add current thread to WDT watch
+
   #ifdef debug
   Serial.begin(115200);
   while(!Serial);
@@ -110,76 +136,124 @@ void setup() {
     Serial.println("Found INA260 chip");
     #endif
   }
+
+  esp_task_wdt_reset();
   
 
   WiFi.mode(WIFI_STA); // set the wifi mode as Station
+  esp_wifi_set_mac(WIFI_IF_STA, &selfMAC[0]);
   if (esp_now_init() != ESP_OK) {
     #ifdef debug
     Serial.println("ESP_Now init failed...");
     #endif
-    delay(RETRY_INTERVAL);
+    delay(3000);
     esp_restart();
+  }else{
+    #ifdef debug
+    Serial.println("ESP_Now init OK...");
+    #endif
+
   }
 
     // Once ESPNow is successfully Init, we will register for Send CB to
     // get the status of Trasnmitted packet
     esp_now_register_send_cb(OnDataSent);
 
-    esp_now_peer_info  receiverinfo;
-    memcpy(receiverinfo.peer_addr, receiverMAC, 6);
-    receiverinfo.channel=0;
-    receiverinfo.encrypt = false;
-
-    // add the receiver module 
-    if( esp_now_add_peer(&receiverinfo) != ESP_OK){
-      #ifdef debug
-      Serial.println("Failed to add the receiver module");
-      #endif
+    memcpy(peerInfo.peer_addr, receiverMAC, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+    
+    // Add peer        
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+      Serial.println("Failed to add peer");
       esp_restart();
     }
   }
 
 void loop() {
 
-  #ifdef PorteArriere
-  uint8_t compteur=0;
-  for(int i=0;i<10;i++){
-    voltage=ina260_1.readBusVoltage();
-    voltage=voltage/1000;
-    if(voltage>TensionSeuil){
-      compteur++;
+/*
+    #ifdef PorteArriere
+    uint8_t compteur=0;
+    for(int i=0;i<10;i++){
+      voltage=ina260_1.readBusVoltage();
+      voltage=voltage/1000;
+      if(voltage>TensionSeuil){
+        compteur++;
+      }
+      delay(5);
     }
-    delay(5);
-  }
 
-  if(compteur>5){
-    bus.PD1='1';
-    #ifdef debug
-    Serial.println("Levée de la porte arrière ON");
-    #endif
-  }else{
-    #ifdef debug
-    Serial.println("Levée de la porte arrière OFF");
-    #endif
-    bus.PD1='0';
-  }
+    if(compteur>5){
+      bus.PD1='1';
+      #ifdef debug
+      Serial.println("Levée de la porte arrière ON");
+      #endif
+    }else{
+      #ifdef debug
+      Serial.println("Levée de la porte arrière OFF");
+      #endif
+      bus.PD1='0';
+    }
 
-  if(refPD1!=bus.PD1){
-    refPD1=bus.PD1;
-    send=true;
-  }
-  if(send){
-    send=false;
-    for(int i=0;i<5;i++){
-      sendData();
-      delay(500);
-      if(SendOK){
-        blinkLed(500,25);
-        delay(3000);
-        break;
+    if(refPD1!=bus.PD1){
+      refPD1=bus.PD1;
+      send=true;
+    }
+    if(send){
+      send=false;
+      for(int i=0;i<5;i++){
+        sendData();
+        delay(500);
+        if(SendOK){
+          blinkLed(500,25);
+          delay(3000);
+          break;
+        }
       }
     }
+    delay(2000);
+    #endif
+
+*/
+
+#ifdef FuelLV
+int comp=0;
+
+  for(int i=0;i<100;i++){
+    waterlv=ina260_1.readBusVoltage();
+    Mwaterlv=Mwaterlv+waterlv;
+    comp++;
+    delay(10);
   }
-  delay(2000);
+
+  Mwaterlv=Mwaterlv/comp; // la moyenne
+
+
+
+  Serial.print("Moyenne=");Serial.println(Mwaterlv);
+
+
+  bus.FuelTank=Mwaterlv;
+  Serial.print("Fuel Lv=");Serial.print(bus.FuelTank);Serial.println(" mV");
+
+  
+  #ifdef debug
+  Serial.println("\nSending Data\n");
   #endif
+  for(int i=0;i<3;i++){
+    sendData();
+    delay(1500);
+    if(SendOK){
+      blinkLed(500,25);
+      break;
+    }
+  }
+
+  esp_task_wdt_reset();
+  delay(10000);
+
+
+#endif
+
 }
